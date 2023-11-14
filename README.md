0406a2042eb4bf31d908e6eef9712e8eb76ec08b
shihabshahrier <shahrier13@yahoo.com>
2023-11-14 07:07:22 +0600

    "done"

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# API_with_flask
+# Flask User Management API
+
+This project is a simple Flask-based user management system, providing a REST API to create, retrieve, and update user information using a SQLite database.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.6 or higher installed on your machine.
+- pip (Python package manager) installed.
+
+## Installing Flask User Management API
+
+To install Flask User Management API, follow these steps:
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/shihabshahrier/API_with_flask.git
+   cd API_with_flask
+    ```
+
+2. Create a virtual environment:
+   ```bash
+   python3 -m venv venv
+   source venv/bin/activate
+   ```
+3. Install the dependencies:
+   ```bash
+    pip install -r requirements.txt
+    ```
+
+## Running the Application
+
+To run the application, follow these steps:
+
+1. Run the application:
+   ```bash
+   python3 app.py
+   ```
+
+## Using the API
+
+The API supports the following endpoints:
+
+- GET /get-user/<username>: Retrieve a user by username.
+- POST /add-user: Add a new user. The request should include username and email in the JSON body.
+- PUT /update/<username>: Update an existing user's email. The request should include email in the JSON body.
---
+++ Simple_API/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+import os
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.String(36), primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(50), unique=True)
+
+
+@app.route("/get-user/<username>", methods=["GET"])
+def get_user(username):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return jsonify({"username": user.username, "email": user.email}), 200
+    else:
+        return jsonify({"message": "User not found"}), 404
+
+
+@app.route("/add-user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    if not data or "username" not in data or "email" not in data:
+        return jsonify({"message": "Bad request, username and email are required"}), 400
+
+    if User.query.filter_by(username=data["username"]).first():
+        return jsonify({"message": "Username already exists"}), 409
+    print(data["username"])
+    new_user = User(
+        id=str(uuid.uuid4()), username=data["username"], email=data["email"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "New user created"}), 201
+
+
+@app.route("/update/<username>", methods=["PUT"])
+def update_user(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    data = request.get_json()
+    if not data or "email" not in data:
+        return jsonify({"message": "Bad request, email is required"}), 400
+
+    user.email = data["email"]
+    db.session.commit()
+    return jsonify({"message": "User updated"}), 200
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
